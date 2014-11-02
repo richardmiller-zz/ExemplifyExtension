97bb5ce0cc0b12cf2b62fb568c85e23b67e0b2a9
richardmiller <rmiller@inviqa.com>
2014-11-02 22:35:21 +0000

    Initial import

---
+++ src/Command/ExemplifyCommand.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace RMiller\ExemplifyExtension\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command line command responsible to signal to generators we will need to
+ * generate a new spec
+ */
+class ExemplifyCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('exemplify')
+            ->setDefinition(array(
+                    new InputArgument('class', InputArgument::REQUIRED, 'Class method belongs to'),
+                    new InputArgument('method', InputArgument::REQUIRED, 'Method to describe'),
+                ))
+            ->setDescription('Adds an example for a method')
+            ->addOption('confirm', null, InputOption::VALUE_NONE, 'Ask for confirmation before creating spec')
+            ->setHelp(<<<EOF
+The <info>%command.name%</info> command creates an example for a method:
+
+  <info>php %command.full_name% ClassName MethodName</info>
+
+Will generate an example in the ClassNameSpec.
+
+EOF
+            )
+        ;
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $container = $this->getApplication()->getContainer();
+        $container->configure();
+
+        $classname = $input->getArgument('class');
+        $method = $input->getArgument('method');
+
+        if (!$this->confirm($input, $container, $classname, $method)) {
+            return;
+        }
+
+        $resource  = $container->get('locator.resource_manager')->createResource($classname);
+
+        $container->get('code_generator')->generate($resource, 'specification_method', array(
+            'method'      => $method,
+        ));
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param $container
+     * @param $classname
+     * @param $method
+     * @return bool
+     */
+    private function confirm(InputInterface $input, $container, $classname, $method)
+    {
+        if (!$input->getOption('confirm')) {
+            return true;
+        }
+
+        $question = sprintf('Do you want to generate an example for %s::%s? (Y/n)', $classname, $method);
+
+        if ($container->get('console.io')->askConfirmation($question, true)) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/Generator/SpecificationMethodGenerator.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace RMiller\ExemplifyExtension\Generator;
+
+use PhpSpec\CodeGenerator\Generator\GeneratorInterface;
+use PhpSpec\Console\IO;
+use PhpSpec\CodeGenerator\TemplateRenderer;
+use PhpSpec\Util\Filesystem;
+use PhpSpec\Locator\ResourceInterface;
+
+/**
+ * Generates class methods from a resource
+ */
+class SpecificationMethodGenerator implements GeneratorInterface
+{
+    /**
+     * @var \PhpSpec\Console\IO
+     */
+    private $io;
+
+    /**
+     * @var \PhpSpec\CodeGenerator\TemplateRenderer
+     */
+    private $templates;
+
+    /**
+     * @var \PhpSpec\Util\Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @param IO               $io
+     * @param TemplateRenderer $templates
+     * @param Filesystem       $filesystem
+     */
+    public function __construct(IO $io, TemplateRenderer $templates, Filesystem $filesystem = null)
+    {
+        $this->io         = $io;
+        $this->templates  = $templates;
+        $this->filesystem = $filesystem ?: new Filesystem();
+    }
+
+    /**
+     * @param ResourceInterface $resource
+     * @param string            $generation
+     * @param array             $data
+     *
+     * @return bool
+     */
+    public function supports(ResourceInterface $resource, $generation, array $data)
+    {
+        return 'specification_method' === $generation;
+    }
+
+    /**
+     * @param ResourceInterface $resource
+     * @param array             $data
+     */
+    public function generate(ResourceInterface $resource, array $data = array())
+    {
+        $filepath  = $resource->getSpecFilename();
+        $method    = $data['method'];
+
+        $values = array('%method%' => $method);
+        if (!$content = $this->templates->render('method', $values)) {
+            $content = $this->templates->renderString(
+                $this->getTemplate(), $values
+            );
+        }
+
+        $code = $this->filesystem->getFileContents($filepath);
+        $code = preg_replace('/}[ \n]*$/', rtrim($content)."\n}\n", trim($code));
+        $this->filesystem->putFileContents($filepath, $code);
+
+        $this->io->writeln(sprintf(
+            "\nExample for <info>Method <value>%s::%s()</value> has been created.</info>",
+            $resource->getSrcClassname(), $method
+        ), 2);
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        return 0;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTemplate()
+    {
+        return file_get_contents(__DIR__.'/templates/specification_method.template');
+    }
+}
