c2c98576f95f36aaedd22ef5e7549cdad6606be4
richardmiller <rmiller@inviqa.com>
2016-09-29 21:05:29 +0100

    Update extension to work with phpspec 3

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -52,7 +52,13 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function theSpecFileShouldContain($file, PyStringNode $string)
     {
-        expect(file_get_contents($file))->toBe($string->getRaw());
+        $actual = file_get_contents($file);
+        $expected = $string->getRaw();
+
+
+        if($actual != $expected) {
+            throw new \RuntimeException("The generated spec file should have been:\n\n$expected\n\nbut was:\n\n$actual\n\n");
+        }
     }
 
     /**
@@ -84,7 +90,7 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     private function createApplicationTester()
     {
-        file_put_contents('phpspec.yml','extensions: [RMiller\ExemplifyExtension\ExemplifyExtension]');
+        file_put_contents('phpspec.yml',"extensions:\n    RMiller\\ExemplifyExtension\\ExemplifyExtension: ~");
         $application = new Application('2.1-dev');
         $application->setAutoExit(false);
 
@@ -105,7 +111,11 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function iShouldSee($message)
     {
-        expect($this->applicationTester->getDisplay())->toMatch('/'.preg_quote($message, '/').'/sm');
+        $actual = trim($this->applicationTester->getDisplay());
+
+        if($actual != $message) {
+            throw new \RuntimeException("The message should have been:\n\n$message\n\nbut was:\n\n$actual\n\n");
+        }
     }
 
     /**
